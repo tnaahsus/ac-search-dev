305bcd10504de526ddc2b13c3ede1131e1617356
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-10-13 19:12:18 +0530

    ..

--- app/db_init.py
+++ app/db_init.py
@@ -12,7 +12,7 @@ posts = [
     ,
     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
     ,
-    "ビットコインは、2008年にサトシ・ナカモトという名前を使った無名の人物またはグループによって発明された、Peer to Peer型の暗号通貨である。この通貨は、その実装がオープンソースソフトウェアとして公開され、2009年に使用が開始された。"
+    "Bitcoin は、2008年にサトシ・ナカモトという名前を使った無名の人物またはグループによって発明された、Peer to Peer型の暗号通貨である。この通貨は、その実装がオープンソースソフトウェアとして公開され、2009年に使用が開始された。"
 ]
 
 
