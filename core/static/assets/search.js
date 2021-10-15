3c25a2b0fcf049afb87e3b7511dc7a6e50202816
Sushant shetty <72088338+tnaahsus@users.noreply.github.com>
2021-10-15 23:48:07 +0530

    Update search.js

--- core/static/assets/search.js
+++ core/static/assets/search.js
@@ -268,51 +268,53 @@ buttton.addEventListener('click', () => {
                     date = datas[i].date;
                     let shortDate = date.slice(0, 10)
                     username = datas[i].username;
-                    let box1 = document.getElementById('commentBox');
-                    let string = `<div class="post-comments card" data=${comment_id}>
-                        <p class="meta">
-
-    <!--                    COMMENT OR POST ICON -->
-                            <i class="far fa-comments"
-                               style="font-size: 1.4rem!important; margin-right: 10px;">
-                            </i>
+                    if(type = 'comment'){
+                        let box1 = document.getElementById('commentBox');
+                        let string = `<div class="post-comments card" data=${comment_id}>
+                            <p class="meta">
     
-    <!--                    USERNAME -->
-                            <i class="fas fa-user"></i>
-                            <a href="https://reddit.com/u/${username}" class="a-comment">
-                                <span class="">${username}</span>
-                            </a>
-                            <span class="mx-1">|</span>
-
-    <!--                     REDDIT LINK-->
-                            <i class="fas fa-sign-out-alt"></i>
-                            <a href="https://reddit.com/r/{{object.sub}}/comments/${post_id}/_/${comment_id}" class="a-comment">
-                                <span class="">View on Reddit</span>
-                            </a>
-                        </p>
-
-    <!--                FULL CONTENT -->
-                    <span id="commentContent" class="full-content text-break"  >
-                    ${shortText}
-                    </span>
+        <!--                    COMMENT OR POST ICON -->
+                                <i class="far fa-comments"
+                                   style="font-size: 1.4rem!important; margin-right: 10px;">
+                                </i>
+        
+        <!--                    USERNAME -->
+                                <i class="fas fa-user"></i>
+                                <a href="https://reddit.com/u/${username}" class="a-comment">
+                                    <span class="">${username}</span>
+                                </a>
+                                <span class="mx-1">|</span>
     
-    <!--                DOTS -->
-                    <span class="dots">...</span>
-                    <span class="more"  style = "display:none">${text}</span>
+        <!--                     REDDIT LINK-->
+                                <i class="fas fa-sign-out-alt"></i>
+                                <a href="https://reddit.com/r/{{object.sub}}/comments/${post_id}/_/${comment_id}" class="a-comment">
+                                    <span class="">View on Reddit</span>
+                                </a>
+                            </p>
     
-    <!--                READ MORE AND DATE -->
-                        <div class="text-end mt-1 mx-2" style="font-size: 0.9rem;">
-                            <button   class="nav-toggle text-muted mx-2 border-0 myBtn" style="cursor:pointer" onclick="myFunction('${comment_id}')"  >
-                                 Read More
-                            </button>|
-                            <span class="text-muted">
-                                ${shortDate}
-                            </span>
-                        </div>
-                    </div>`
-                    let commentElement = getElementFromString(string);
-                    // console.log(parameterElement);
-                    box1.appendChild(commentElement)
+        <!--                FULL CONTENT -->
+                        <span id="commentContent" class="full-content text-break"  >
+                        ${shortText}
+                        </span>
+        
+        <!--                DOTS -->
+                        <span class="dots">...</span>
+                        <span class="more"  style = "display:none">${text}</span>
+        
+        <!--                READ MORE AND DATE -->
+                            <div class="text-end mt-1 mx-2" style="font-size: 0.9rem;">
+                                <button   class="nav-toggle text-muted mx-2 border-0 myBtn" style="cursor:pointer" onclick="myFunction('${comment_id}')"  >
+                                     Read More
+                                </button>|
+                                <span class="text-muted">
+                                    ${shortDate}
+                                </span>
+                            </div>
+                        </div>`
+                        let commentElement = getElementFromString(string);
+                        // console.log(parameterElement);
+                        box1.appendChild(commentElement)
+                    }
 
 
 
@@ -368,69 +370,68 @@ buttton.addEventListener('click', () => {
                     date = datas[i].date;
                     let shortDate = date.slice(0, 10)
                     username = datas[i].username;
-
-                    // console.log(s)
-                    // console.log(type)
-                    let box2 = document.getElementById('postBox');
-                    let string = ` <div class="post-comments card"  data=${post_id} >
-                            <p class="meta">
-        
-        <!--                    COMMENT OR POST ICON -->
-                                <i class="far fa-newspaper"
-                                   style="font-size: 1.4rem!important; margin-right: 10px;">
-                                </i>
-        
-        <!--                    USERNAME -->
-                                <i class="fas fa-user"></i>
-                                <a href="https://reddit.com/u/${username}" class="a-comment">
-                                    <span class="">${username}</span>
-                                </a>
-                                <span class="mx-1">|</span>
-        
-        <!--                     REDDIT LINK-->
-                                <i class="fas fa-sign-out-alt"></i>
-                                <a href="https://reddit.com/r/sub/comments/${post_id}" class="a-comment">
-                                    <span class="">View on Reddit</span>
-                                </a>
-                            </p>
-                            <div class="row">
-        
-        <!--                    THUMBNAIL -->
-                                <div class="col-3  text-center">
-                                    <img class="img thumbnail" width='75px' height="55px"
-                                         src="https://b.thumbs.redditmedia.com/qTtigz4VcFX0pzdOJnG5HLKvLt3uoEWr-vZ0rvNEteQ.jpg"  alt="post-image"/>
-                                    <p class="text-muted mb-0 p-0">
-                                        <small class="mx-1 "><i class="fas fa-thumbs-up"></i></small>
-                                        <small class="mt-1">86</small>
-                                    </p>
-                                </div>
-        
-                                <div class="col-9">
-                <!--                FULL CONTENT -->
-                                    <span id="postContent" class="full-content text-break"  >
-                                   ${shortText}
-                                    </span>
-           
-                <!--                DOTS -->
-                                   <span class="dots">...</span>
-                                   <span class="more"  style = "display:none">${text}</span>
-        
-        
-                <!--                READ MORE AND DATE -->
-                                    <div class="text-end mt-1 mx-2" style="font-size: 0.9rem;">
-                                        <button   class="nav-toggle text-muted mx-2 border-0 myBtn" style="cursor:pointer" onclick="myFunction('${post_id}')"  >
-                                            Read More
-                                        </button>|
-                                        <span class="text-muted">
-                                            ${shortDate}
+                    if(type == 'post'){
+                        let box2 = document.getElementById('postBox');
+                        let string = ` <div class="post-comments card"  data=${post_id} >
+                                <p class="meta">
+            
+            <!--                    COMMENT OR POST ICON -->
+                                    <i class="far fa-newspaper"
+                                       style="font-size: 1.4rem!important; margin-right: 10px;">
+                                    </i>
+            
+            <!--                    USERNAME -->
+                                    <i class="fas fa-user"></i>
+                                    <a href="https://reddit.com/u/${username}" class="a-comment">
+                                        <span class="">${username}</span>
+                                    </a>
+                                    <span class="mx-1">|</span>
+            
+            <!--                     REDDIT LINK-->
+                                    <i class="fas fa-sign-out-alt"></i>
+                                    <a href="https://reddit.com/r/sub/comments/${post_id}" class="a-comment">
+                                        <span class="">View on Reddit</span>
+                                    </a>
+                                </p>
+                                <div class="row">
+            
+            <!--                    THUMBNAIL -->
+                                    <div class="col-3  text-center">
+                                        <img class="img thumbnail" width='75px' height="55px"
+                                             src="https://b.thumbs.redditmedia.com/qTtigz4VcFX0pzdOJnG5HLKvLt3uoEWr-vZ0rvNEteQ.jpg"  alt="post-image"/>
+                                        <p class="text-muted mb-0 p-0">
+                                            <small class="mx-1 "><i class="fas fa-thumbs-up"></i></small>
+                                            <small class="mt-1">86</small>
+                                        </p>
+                                    </div>
+            
+                                    <div class="col-9">
+                    <!--                FULL CONTENT -->
+                                        <span id="postContent" class="full-content text-break"  >
+                                       ${shortText}
                                         </span>
+               
+                    <!--                DOTS -->
+                                       <span class="dots">...</span>
+                                       <span class="more"  style = "display:none">${text}</span>
+            
+            
+                    <!--                READ MORE AND DATE -->
+                                        <div class="text-end mt-1 mx-2" style="font-size: 0.9rem;">
+                                            <button   class="nav-toggle text-muted mx-2 border-0 myBtn" style="cursor:pointer" onclick="myFunction('${post_id}')"  >
+                                                Read More
+                                            </button>|
+                                            <span class="text-muted">
+                                                ${shortDate}
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>`
-                    let postElement = getElementFromString(string);
-                    // console.log(parameterElement);
-                    box2.appendChild(postElement);
+                            </div>`
+                        let postElement = getElementFromString(string);
+                        // console.log(parameterElement);
+                        box2.appendChild(postElement);
+                    }
 
                 }
                 // console.log('post')
